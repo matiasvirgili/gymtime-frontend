7f59d9a2bf80f2ddc2c9b4611f2bb424c4254125
Matias Virgili <matiasvirgili@gmail.com>
2022-08-29 20:56:12 -0300

    redux workout event

--- src/redux/actions/workoutEventAction.js
+++ src/redux/actions/workoutEventAction.js
@@ -1,1 +1,138 @@
-console.log(first);
+import axios from 'axios';
+import {
+  WORKOUTEVENT_CREATE_WORKOUTEVENT,
+  WORKOUTEVENT_UPDATE_WORKOUTEVENT,
+  WORKOUTEVENT_DELETE_WORKOUTEVENT,
+  WORKOUTEVENT_SET_ALL_WORKOUTEVENTS,
+  WORKOUTEVENT_SET_ERROR,
+  WORKOUTEVENT_SET_LOADING_TRUE,
+  WORKOUTEVENT_SET_CREATE_ACTION,
+  WORKOUTEVENT_SET_UPDATE_ACTION,
+  WORKOUTEVENT_SET_DELETE_ACTION,
+  WORKOUTEVENT_UNSET_ACTION,
+} from '../types/workoutEventTypes';
+import { getConfig } from '../../helpers/axiosConfig';
+
+export const createWorkoutEvent = (workoutEvent) => {
+  return {
+    type: WORKOUTEVENT_CREATE_WORKOUTEVENT,
+    payload: workoutEvent,
+  };
+};
+
+export const updateWorkoutEvent = (workoutEvent) => {
+  return {
+    type: WORKOUTEVENT_UPDATE_WORKOUTEVENT,
+    payload: workoutEvent,
+  };
+};
+export const deleteWorkoutEvent = (workoutEventId) => {
+  return {
+    type: WORKOUTEVENT_DELETE_WORKOUTEVENT,
+    payload: workoutEventId,
+  };
+};
+export const setWorkoutEvents = (workoutEvents) => {
+  return {
+    type: WORKOUTEVENT_SET_ALL_WORKOUTEVENTS,
+    payload: workoutEvents,
+  };
+};
+export const setError = (error) => {
+  return {
+    type: WORKOUTEVENT_SET_ERROR,
+    payload: error,
+  };
+};
+export const setLoadingTrue = () => {
+  return {
+    type: WORKOUTEVENT_SET_LOADING_TRUE,
+  };
+};
+export const setCreateAction = () => {
+  return {
+    type: WORKOUTEVENT_SET_CREATE_ACTION,
+  };
+};
+export const setDeleteAction = (workoutEvent) => {
+  return {
+    type: WORKOUTEVENT_SET_DELETE_ACTION,
+    payload: workoutEvent,
+  };
+};
+export const setUpdateAction = (workoutEvent) => {
+  return {
+    type: WORKOUTEVENT_SET_UPDATE_ACTION,
+    payload: workoutEvent,
+  };
+};
+export const unsetAction = () => {
+  return {
+    type: WORKOUTEVENT_UNSET_ACTION,
+  };
+};
+
+export const getWorkoutEventsAsync = () => async (dispatch) => {
+  try {
+    const res = await axios.get(
+      // eslint-disable-next-line no-undef
+      `${process.env.REACT_APP_BACKEND_URL_PORT}/workoutevents`
+    );
+    if (res.status === 200) {
+      let workoutEvents = [];
+      for (let i = 0; i < res.data.length; i++) {
+        workoutEvents.push(res.data[i]);
+      }
+      dispatch(setWorkoutEvents(workoutEvents));
+    }
+  } catch (error) {
+    dispatch(setError(error?.response?.data?.error));
+  }
+};
+export const deleteWorkoutEventAsync = (workoutEventId) => async (dispatch) => {
+  dispatch(setLoadingTrue());
+  try {
+    const res = await axios.delete(
+      // eslint-disable-next-line no-undef
+      `${process.env.REACT_APP_BACKEND_URL_PORT}/workoutevents/${workoutEventId}`,
+      getConfig()
+    );
+    if (res.status === 200) {
+      dispatch(deleteWorkoutEvent(workoutEventId));
+    }
+  } catch (error) {
+    dispatch(setError(error?.response?.data?.error));
+  }
+};
+export const createWorkoutEventAsync = (workoutEvent) => async (dispatch) => {
+  dispatch(setLoadingTrue());
+  try {
+    const res = await axios.post(
+      // eslint-disable-next-line no-undef
+      `${process.env.REACT_APP_BACKEND_URL_PORT}/workoutevents`,
+      workoutEvent,
+      getConfig()
+    );
+    if (res.status === 201) {
+      return dispatch(createWorkoutEvent(res.data.data));
+    }
+  } catch (error) {
+    return dispatch(setError(error?.response?.data?.error));
+  }
+};
+export const updateWorkoutEventAsync = (workoutEvent) => async (dispatch) => {
+  dispatch(setLoadingTrue());
+  try {
+    const res = await axios.put(
+      // eslint-disable-next-line no-undef
+      `${process.env.REACT_APP_BACKEND_URL_PORT}/workoutevent/${workoutEvent._id}`,
+      workoutEvent,
+      getConfig()
+    );
+    if (res.status === 200) {
+      return dispatch(updateWorkoutEvent(res.data.data));
+    }
+  } catch (error) {
+    return dispatch(setError(error?.response?.data?.error));
+  }
+};
--- src/redux/reducers/workoutEventReducer.js
+++ src/redux/reducers/workoutEventReducer.js
@@ -1,1 +1,103 @@
-console.log(first);
+import {
+  WORKOUTEVENT_CREATE_WORKOUTEVENT,
+  WORKOUTEVENT_UPDATE_WORKOUTEVENT,
+  WORKOUTEVENT_DELETE_WORKOUTEVENT,
+  WORKOUTEVENT_SET_ALL_WORKOUTEVENTS,
+  WORKOUTEVENT_SET_ERROR,
+  WORKOUTEVENT_SET_LOADING_TRUE,
+  WORKOUTEVENT_CREATE_ACTION,
+  WORKOUTEVENT_SET_UPDATE_ACTION,
+  WORKOUTEVENT_SET_DELETE_ACTION,
+  WORKOUTEVENT_UNSET_ACTION,
+} from '../types/workoutEventTypes';
+
+import { UPDATE, DELETE, CREATE, NONE } from '../types/modalTypes';
+
+const initialState = {
+  list: [],
+  error: '',
+  isLoading: false,
+  actionInProgress: NONE,
+  selectedWorkoutEvent: null,
+  credentials: {
+    workoutEvent: null,
+    token: '',
+  },
+};
+
+export const workoutEventReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case WORKOUTEVENT_CREATE_WORKOUTEVENT:
+      return {
+        ...state,
+        error: '',
+        list: [...state.list, action.payload],
+        isLoading: false,
+        actionInProgress: NONE,
+        selectedWorkoutEvent: null,
+      };
+    case WORKOUTEVENT_UPDATE_WORKOUTEVENT:
+      return {
+        ...state,
+        error: '',
+        list: state.list.map((workoutEvent) =>
+          workoutEvent._id === action.payload._id
+            ? action.payload
+            : workoutEvent
+        ),
+        isLoading: false,
+        actionInProgress: NONE,
+        selectedWorkoutEvent: null,
+      };
+    case WORKOUTEVENT_DELETE_WORKOUTEVENT:
+      return {
+        ...state,
+        error: '',
+        list: state.list.filter(
+          (workoutEvent) => workoutEvent._id !== action.payload
+        ),
+        isLoading: false,
+        actionInProgress: NONE,
+        selectedWorkoutEvent: null,
+      };
+    case WORKOUTEVENT_SET_ALL_WORKOUTEVENTS:
+      return { ...state, list: action.payload, error: '', isLoading: false };
+    case WORKOUTEVENT_SET_ERROR:
+      return {
+        ...state,
+        error: action.payload || 'An error ocurred',
+        isLoading: false,
+      };
+    case WORKOUTEVENT_SET_LOADING_TRUE:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case WORKOUTEVENT_CREATE_ACTION:
+      return {
+        ...state,
+        actionInProgress: CREATE,
+      };
+    case WORKOUTEVENT_SET_UPDATE_ACTION:
+      return {
+        ...state,
+        actionInProgress: UPDATE,
+        selectedWorkoutEvent: { ...action.payload },
+      };
+    case WORKOUTEVENT_SET_DELETE_ACTION:
+      return {
+        ...state,
+        actionInProgress: DELETE,
+        selectedWorkoutEvent: { ...action.payload },
+      };
+    case WORKOUTEVENT_UNSET_ACTION:
+      return {
+        ...state,
+        actionInProgress: NONE,
+        selectedWorkoutEvent: null,
+        error: '',
+      };
+    default:
+      return state;
+  }
+};
